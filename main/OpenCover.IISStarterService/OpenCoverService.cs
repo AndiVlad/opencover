2d29a692c5a082fca1d860d447ca3d4739a5d30d
AndiVlad <mihai.vlad@gmail.com>
2014-11-28 11:47:35 +0200

    IIS starter service
    When using for a longer time on a machine that may restart etc...

---
+++ main/OpenCover.IISStarterService/OpenCoverService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Layout;
+
+namespace OpenCover.IISStarterService
+{
+    partial class OpenCoverService : ServiceBase
+    {
+        private Thread _executionThread;
+        private volatile bool _isStopping;
+        private ILog _logger;
+        
+        public OpenCoverService()
+        {
+            InitializeComponent();
+
+            _isStopping = false;
+
+            #region init event log
+            var layout = new PatternLayout("%utcdate %-5level - %message%newline");
+            layout.ActivateOptions();
+            var appender = new EventLogAppender() { Layout = layout, ApplicationName = "OpenCover" };
+            appender.ActivateOptions();
+            BasicConfigurator.Configure(appender);
+            var root = ((log4net.Repository.Hierarchy.Hierarchy)LogManager.GetRepository()).Root;
+            root.AddAppender(appender);
+
+            _logger = LogManager.GetLogger(typeof(OpenCoverService));
+            #endregion
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Start WWWW
+            ServiceController sc = new ServiceController("w3svc");
+            if (sc.Status != ServiceControllerStatus.Stopped)
+            {
+                sc.Stop();
+            }
+
+            // Set the variables
+            var coverageDir = Environment.GetEnvironmentVariable("CoverageDir") ?? @"C:\Coverage";
+            Environment.CurrentDirectory = coverageDir;
+
+            // Work on the execution thread
+            _executionThread = new Thread(new ThreadStart(() =>
+            {
+                Debugger.Launch();
+
+                while (!_isStopping)
+                {
+                    // Start the coverage
+                    var output = string.Format("-output:coverage_{0:yyyy-MM-dd_hh-mm-ss-tt}.xml", DateTime.Now);
+                    var par = new List<string>() { "-service", "-target:w3svc", output };
+
+                    // start the action
+                    _logger.InfoFormat("Starting OpenCover with arguments: {0} ", string.Join(" ", par));
+                    OpenCover.Console.Program.Main(par.ToArray());
+                    _logger.InfoFormat("OpenCover Stopped");
+                }
+            }));
+            _executionThread.Start();
+        }
+
+        protected override void OnStop()
+        {
+            _logger.InfoFormat("Stop requested");
+            _isStopping = true;
+
+            // STOP WWW
+            ServiceController sc = new ServiceController("w3svc");
+            if (sc.Status != ServiceControllerStatus.Stopped)
+            {
+                sc.Stop();
+            }
+
+            // Wait for joining
+            _executionThread.Join();
+        }
+
+        protected override void OnShutdown()
+        {
+            OnStop();
+        }
+    }
+}
---
+++ main/OpenCover.IISStarterService/ProjectInstaller.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace OpenCover.IISStarterService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenCoverServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.OpenCoverServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // OpenCoverServiceProcessInstaller
+            // 
+            this.OpenCoverServiceProcessInstaller.Password = null;
+            this.OpenCoverServiceProcessInstaller.Username = null;
+            // 
+            // OpenCoverServiceInstaller
+            // 
+            this.OpenCoverServiceInstaller.ServiceName = "OpenCoverService";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.OpenCoverServiceProcessInstaller,
+            this.OpenCoverServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller OpenCoverServiceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller OpenCoverServiceInstaller;
+    }
+}
